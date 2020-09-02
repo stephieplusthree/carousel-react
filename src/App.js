de8932a536c7226431518a381c2cda14bd65ea5c
stephanie <stephieplusthree@gmail.com>
2020-09-02 15:30:02 -0700

    slider added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import 'bootstrap/dist/css/bootstap:css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './App.css';
+import Slider from 'react-slick';
 
 function App() {
+  const [suggestions,setSuggestions]=useState([])
+
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users').then(res=>res.json()).then(data=>{
+      setSuggestions(data);
+    })
+      });
+
+      let settings = {
+        infinite: false,
+        speed: 1000,
+        arrows:true,
+        slidesToShow: 5,
+        slidesToScroll: 4,
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h6 className="text-muted">Friend Suggestions</h6>
+      {suggestions.length === 0 ? (
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading ...</span>
+          </div>
+      ) : (
+          <Slider {...settings}>
+          {suggestions.map((current => (
+            <div className="out" key={current.id}>
+              <div className="card">
+                <img 
+                  className="rounded-circle" 
+                  alt={"users here"} 
+                  src={`https://source.unsplash.com/random/${current.id}`}
+                  height={56}
+                  width={56}
+                />
+              </div>
+            </div>
+          ))}
+          </Slider>
+      )}
     </div>
   );
 }
